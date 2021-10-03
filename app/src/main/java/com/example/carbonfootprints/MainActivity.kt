62f044b4096c4b91aff7bbdf440697e20ac63b41
Joseph Balascio <55389677+SU-Palm@users.noreply.github.com>
2021-10-03 00:28:55 -0400

    Finished Login UI, Status Bar broken coloring scheme need to fix.

--- app/src/main/java/com/example/carbonfootprints/MainActivity.kt
+++ app/src/main/java/com/example/carbonfootprints/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.carbonfootprints
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+
 }
