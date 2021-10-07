a62a3d617c5a26dccd40b0797c457d4df652f212
Fiona PB <59042926+fcpowe@users.noreply.github.com>
2021-10-07 18:52:47 -0400

    Added signup and login functionality NOTE: need to update skipToHome() in MainActivity.java, signup() in Signup.java and login() in Login.java with whatever the dashboard class name is

--- app/src/main/java/com/example/carbonfootprints/MainActivity.java
+++ app/src/main/java/com/example/carbonfootprints/MainActivity.java
@@ -5,15 +5,32 @@ import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
         this.setListeners();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            skipToHome();
+        }
+    }
     private void setListeners() {
         Button loginButton = findViewById(R.id.loginButton);
         Button signUpButton = findViewById(R.id.signUpButton);
@@ -27,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signUp() {
+        Intent intent = new Intent(this, Signup.class);
+        this.startActivity(intent);
+    }
+    private void skipToHome() {
+        //REPLACE WITH DASHBOARD CLASS
         Intent intent = new Intent(this, Signup.class);
         this.startActivity(intent);
     }
